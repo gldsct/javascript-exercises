059ddab46b3f2553bf36e5eb6e25c58e7f50b761
gldsct <188839581+gldsct@users.noreply.github.com>
2024-11-23 17:54:23 +0545

    Create function to remove items from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-};
+// The commented code below is my original solution.
+
+// const removeFromArray = function(arrayReceived) {
+//     let arrayCopy = arrayReceived.map(x => x);
+    
+//     arrayReceived.forEach((item) => {
+//         for (i = 1; i < arguments.length; i++) {
+//             if (item === arguments[i]) {
+//                 arrayCopy.splice(arrayCopy.indexOf(item), 1);
+//             }
+//         }
+//     });
+//     return arrayCopy;
+// };
+
+// The unedited code below is my solution after referring to the official solution and documentation.
+
+const removeFromArray = function(array, ...args) {
+    return array.filter(arrayItem => !args.includes(arrayItem));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
