bca8964655d5d141cf020aed23f6c062cfe3b559
gldsct <188839581+gldsct@users.noreply.github.com>
2024-11-23 19:53:26 +0545

    Sum consecutive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,49 @@
-const sumAll = function() {
+// Commented out below is the original code I wrote.
 
-};
+// const sumAll = function(firstNumber, secondNumber) {
+//     let sum = 0;
+
+//     if (firstNumber < 0 || secondNumber < 0) {
+//         return "ERROR";
+//     }
+
+//     if (Math.floor(firstNumber) !== firstNumber || Math.floor(secondNumber) !== secondNumber) {
+//         return "ERROR";
+//     }
+
+//     smallerNumber = (firstNumber < secondNumber) ? firstNumber : secondNumber;
+//     largerNumber = (firstNumber > secondNumber) ? firstNumber : secondNumber;
+
+//     for (let i = smallerNumber; i <= largerNumber; i++) {
+//         sum += i;
+//     }
+//     return sum;
+// };
+
+// The new code, after consulting with official solution and documentation is:
+
+const sumAll = function(firstNumber, secondNumber) {
+    if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
+
+    if (Number.isInteger(firstNumber) === false || Number.isInteger(secondNumber) === false) {
+        return "ERROR";
+    }
+
+    if (firstNumber > secondNumber) {
+        let temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+    }
+
+    let sum = 0;
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
