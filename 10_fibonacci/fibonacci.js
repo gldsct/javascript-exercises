055f96a27fc39bda858e36f4a624c06c83e80e21
gldsct <188839581+gldsct@users.noreply.github.com>
2025-04-16 12:28:59 +0545

    Get n-th fibonnaci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fib = [];
+    fib[0] = 1;
+    fib[1] = 1;
+    if (num > 0) {
+        for (let i = 2; i < num; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib[num-1];
+    }
+    else if (num == 0) return 0;
+    else return "OOPS";
 };
 
 // Do not edit below this line
