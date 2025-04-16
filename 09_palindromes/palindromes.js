f5137529aac6d62b3686e5611ac111f0c8ee5ac7
gldsct <188839581+gldsct@users.noreply.github.com>
2025-04-16 12:02:33 +0545

    Check if string is palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // str = str.replaceAll(" ", "")
+    // .replaceAll("'", "")
+    // .replaceAll(".", "")
+    // .replaceAll(",", "")
+    // .replaceAll("!", "");
+    // let arr = str.split("");
+    // arr.reverse();
+    // return (str.toLowerCase() === arr.join("").toLowerCase());
 
+    let cleanCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    let cleanedString = str
+    .toLowerCase()
+    .split("")
+    .filter((item) => (cleanCharacters.includes(item)))
+    .join("");
+
+    let reversedString = cleanedString.split("").reverse().join("");
+
+    return (cleanedString === reversedString);
 };
 
 // Do not edit below this line
