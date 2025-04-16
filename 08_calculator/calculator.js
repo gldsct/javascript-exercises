cc866f02a6b051e9334a38ac8b6757fb6e646cc1
gldsct <188839581+gldsct@users.noreply.github.com>
2025-04-16 11:15:43 +0545

    Add calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, currentItem) => {
+    return accumulator + currentItem;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, currentItem) => {
+    return accumulator * currentItem;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorialResult = 1;
+  for (let i = num; i > 1; i--) {
+    factorialResult = factorialResult * i;
+  }
+  return factorialResult;
 };
 
 // Do not edit below this line
